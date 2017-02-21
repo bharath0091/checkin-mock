c74f93ad4ed1c6766644a453b6cc64e37f049fa2
Bharath Kumar Chennupati <bharath.chennupati@klm.com>
2017-02-21 18:29:43 +0530

    completed labels get service implementation

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
 var express = require('express');
 var path = require('path');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var labels = require('./routes/i18n');
-
-
 var app = express();
 
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// app.use('/newapp/labels', require('./routes/i18n'));
+app.use('/newapp/labels', require('./routes/i18n'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -32,8 +26,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500).end();
 });
 
 module.exports = app;
--- routes/i18n.js
+++ routes/i18n.js
@@ -1,17 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var bodyParser = require('body-parser');
+router.use(bodyParser.json());
+
 let i18n = require("../i18n");
 
-router.get('/:language', function(req, res) {
+router.get('/language/:language', function(req, res) {
+    var a = null;
+    a.toString();
     let lables = i18n(req.params.language);
     if (lables) {
         res.writeHead(200, "Ok", {"Context-Type": "json/plain"});
-        res.end(lables);
+        res.end(JSON.stringify(lables));
     } else {
         res.status(404).end();
     }
-})
+});
 
 
 module.exports = router;
